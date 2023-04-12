f961a5f94a5e0be215d45048f9b16936d0d8079d
Valery Zonov <valery.zonov@sibers.com>
2023-04-12 16:54:15 +0700

    prectice

--- app.ts
+++ app.ts
@@ -1,16 +1,11 @@
-function getFullName(userEntity: {firstName: string, surName: string}): string {
-	return `${userEntity.firstName} ${userEntity.surName}`
-}
-
-const user = {
-	firstName: 'Valery',
-	surName: 'Zonov',
-	city: 'Barnaul',
-	age: 23,
-	skills: {
-		dev: true,
-		devops: true,
+let info: {
+	officeId: number;
+	isOpened: boolean;
+	contacts: {
+		phone: string;
+		email: string;
+		address: {
+			city: string;
+		}
 	}
 }
-
-console.log(getFullName(user));
